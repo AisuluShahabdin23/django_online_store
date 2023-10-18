9753d4db6062eeea5734cf2851b4900e175441e4
Aisulu Shahabdin <aisulu_sh_98@mail.ru>
2023-10-18 15:24:07 +0600

    add new app

---
+++ catalog/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+
+
+def home(request):
+    return render(request, template_name='catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Name: {name}\n Phone: {phone}\n Message: {message}')
+    return render(request, template_name='catalog/contacts.html')
